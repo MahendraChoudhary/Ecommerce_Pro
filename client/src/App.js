bfa26bda8f3fbd2f36a9d7cedc492ffcd347d3b7
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2024-01-18 19:16:06 +0530

    added all necessary files related to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,60 @@
-import './App.css';
+// import logo from './logo.svg';
+// import ComponentA from './ClassComponents/ComponentA';
+// import ComponentA from './FC/ComponentA';
+// import Flexbox from './Flexbox';
+// import BootstrapDemo from './BootstrapDemo';
+// import Counter from './Counter';
+// import './App.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+
+import MyNavbar from "./MyNavbar";
+import Login from "./Login";
+import Signup from "./Signup";
+import Products from "./Products";
+import Loader from "./Loader";
+import Toast from "./Toast";
+import { loginWithCookieActionCreator } from "./reducers/userReducer";
+// import CartComponent from "./CartComponent";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("test");
+  const [showComponent, setshowComponent] = useState(true);
+  const prop1 = "abcd";
+
+  useEffect(() => {
+    dispatch(loginWithCookieActionCreator());
+  }, []);
+
+  // const name = 'Nikhil';
   return (
-    <h1>Hii</h1>
+    <>
+      {/* <button onClick={() => setshowComponent(!showComponent)}>{showComponent ? 'unmount ' : 'mount '}component</button>
+      <button onClick={() => setName('Nikhil')}>Update Name</button>
+      {showComponent ? <ComponentA name={name} prop1={prop1} /> : null} */}
+      {/* <Flexbox /> */}
+      {/* <BootstrapDemo /> */}
+      <BrowserRouter>
+        <MyNavbar />
+        <Loader />
+        <Toast />
+        <Routes>
+          <Route path="" element={<Products />} />
+          {/* <Route path='flex' element={<Flexbox />} />
+          <Route path='bootstrap' element={<BootstrapDemo />} /> */}
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          {/* <Route path='counter' element={<Counter />} /> */}
+          {/* <Route path="cart" element={<CartComponent />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
